9a701a6bd6524ac25cdb93a860ced0d9d04ad4f3
achbj <bj.ach7@gmail.com>
2022-12-01 00:37:11 +0545

    minor changes

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -6,6 +6,6 @@ export const headerData = {
   desciption:
     "There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
   image:
-    "https://drive.google.com/file/d/1mZAaEv2DBOjjEVYxy2GGVYuBBAB7kX9U/view?usp=sharing",
+    "https://cloud.eradcorporation.com/wl/?id=MezG0dFzl6UErgV4C08Dwjr6z9z9MUeG",
   resumePdf: resume,
 };
