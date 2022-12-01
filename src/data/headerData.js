586abf4d7e13ab1d246596d89c7f5002fd8336d5
achbj <bj.ach7@gmail.com>
2022-12-01 14:31:03 +0545

    change in header

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,8 +3,7 @@ import resume from "../assets//pdf/resume.pdf";
 export const headerData = {
   name: "Bj Acharya",
   title: "ML Engineer",
-  desciption:
-    "Hello , Thank you for taking the time to visit. I specialize in Machine Learning and AI. Fill your Senses with the highlights of work, read the juicy bits about me, or find out why you should hire me.",
+  desciption: `Hello , Thank you for taking the time to visit. I specialize in Machine Learning and AI. Fill your Senses with the <a href="#experience">highlights of work</a>, read the juicy bits <a href="#about">about me</a>, or find out why you should <a href="#projects">hire me</a>.`,
   image:
     "https://cloud.eradcorporation.com/wl/?id=MezG0dFzl6UErgV4C08Dwjr6z9z9MUeG",
   resumePdf: resume,
