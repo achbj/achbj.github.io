bc9b1b60db2ed2ca1adbca77e27e9a98a722bee3
achbj <bj.ach7@gmail.com>
2022-12-01 14:13:30 +0545

    changes in experience

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -6,4 +6,18 @@ export const experienceData = [
     startYear: "2022",
     endYear: "--",
   },
+  {
+    id: 2,
+    company: "Web Project - Wordpress",
+    jobtitle: "Web Developer",
+    startYear: "2020",
+    endYear: "2020",
+  },
+  {
+    id: 3,
+    company: "Own Project For Meme Sharing & using ML",
+    jobtitle: "Web Developer & ML Engineer",
+    startYear: "2018",
+    endYear: "__",
+  },
 ];
