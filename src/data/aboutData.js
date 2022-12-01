820274234c8571d2e964def1ae436afcf0ceae5c
achbj <bj.ach7@gmail.com>
2022-12-01 14:24:24 +0545

    about

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -3,6 +3,6 @@ export const aboutData = {
   description1:
     "My name's Bijay. I'm a ML/AI Engineer based in Nepal, Kathmandu.",
   description2:
-    "Nice to meet You!, As a results driven individual with a interest in machine learning and AI, I am working as machine learning engineer since this september. Currently, I am working as Intern in AI Marga Pvt. Ltd. I am experienced in data visualization, predictive analysis, statistical modeling and machine learning algorithms.",
+    "Once Again, Nice to meet You!, As a results driven individual with a interest in machine learning and AI, I am working as machine learning engineer since this september. Currently, I am working as Intern in AI Marga Pvt. Ltd. I am experienced in data visualization, predictive analysis, statistical modeling and machine learning algorithms.",
   image: 2,
 };
