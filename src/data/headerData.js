69c563be27dbb200ee2c178e7ce33b0d5b2a53fd
achbj <bj.ach7@gmail.com>
2024-01-11 11:35:43 +0545

    data changes

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,11 +1,11 @@
 import resume from "../assets//pdf/resume.pdf";
+import wp from "../assets/jpeg/wp.jpeg";
 
 export const headerData = {
   name: "Bj Acharya",
   title: "ML Engineer",
   desciption:
     'Hello , Thank you for taking the time to visit. I specialize in Machine Learning and AI. Fill your Senses with the <a href="#experience">highlights of work</a>, read the juicy bits <a href="#about">about me</a>, or find out why you should <a href="#projects">hire me</a>.',
-  image:
-    "https://cloud.eradcorporation.com/wl/?id=MezG0dFzl6UErgV4C08Dwjr6z9z9MUeG",
+  image: wp,
   resumePdf: resume,
 };
