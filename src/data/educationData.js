29c43e5802046c3ef93fb35e97a46a0753f771c9
achbj <bj.ach7@gmail.com>
2024-02-06 11:33:58 +0545

    education

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,13 +1,13 @@
 export const educationData = [
+  // {
+  //   id: 1,
+  //   institution: `Golden Gate Int'l College`,
+  //   course: "Higher Secondary Education",
+  //   startYear: "2017",
+  //   endYear: "2019",
+  // },
   {
     id: 1,
-    institution: `Golden Gate Int'l College`,
-    course: "Higher Secondary Education",
-    startYear: "2017",
-    endYear: "2019",
-  },
-  {
-    id: 2,
     institution: "Coventry University",
     course: "BSc (hons) Computing",
     startYear: "2019",
