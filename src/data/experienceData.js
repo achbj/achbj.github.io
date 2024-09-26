a7b790fbecc4e60a56441ac6f3bb4052022e24b0
achbj <bj.ach7@gmail.com>
2024-09-26 16:47:50 +0545

    experience

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,31 @@
 export const experienceData = [
   {
     id: 1,
-    company: "Intern",
-    jobtitle: "ML Engineer",
-    startYear: "2022",
+    company: "NAAMII",
+    jobtitle: "Research Intern",
+    startYear: "2024",
     endYear: "",
   },
   {
     id: 2,
-    company: "Web Project - Wordpress",
-    jobtitle: "Web Developer",
-    startYear: "2020",
-    endYear: "2020",
+    company: "Global Tech Nepal Pvt. Ltd",
+    jobtitle: "Junior ML Engineer",
+    startYear: "2023",
+    endYear: "",
+  },
+
+  {
+    id: 2,
+    company: "Kush Fintech Pvt. Ltd",
+    jobtitle: "Data Analysis Intern",
+    startYear: "2023",
+    endYear: "2023",
   },
   {
     id: 3,
-    company: "My Project For Meme Sharing & using ML on data",
-    jobtitle: "Web Developer & ML Engineer",
-    startYear: "2018",
-    endYear: "",
+    company: "Free Lance",
+    jobtitle: "Web Development (MERN, Django)",
+    startYear: "2019",
+    endYear: "2022",
   },
 ];
